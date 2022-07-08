ff57f637ae1b94621dbef789e388d96ad0470896
MarketingPip <86180097+MarketingPip@users.noreply.github.com>
2022-07-08 03:27:28 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,26 +1,5 @@
-#!/bin/bash
-#####################################################
-#  Download Specific folders from Github using SVN
-#  
-#  Author: Declan Cook
-#  Licence: MIT
-#####################################################
-GHDOMAIN="https://github.com/"
-IN=${1}
-IN=${IN##$GHDOMAIN}
-BRANCH="trunk"
-FOLDER=""
-IFS="/" read -a SECT <<< "$IN" 
+#!/bin/sh -l
 
-if [[ "${SECT[3]}" != "master" ]]; then
-  BRANCH=${SECT[3]}
-fi
-for index in "${!SECT[@]}"; do
-  if [ $index -gt 3 ]; then
-    FOLDER=$FOLDER/${SECT[index]}
-  fi
-done
-
-# DOMAIN/USER/PROJECT/<TRUNK||BRANCH>/FOLDER
-echo Exporting $GHDOMAIN${SECT[0]}/${SECT[1]}/$BRANCH$FOLDER
-svn --force export $GHDOMAIN${SECT[0]}/${SECT[1]}/$BRANCH$FOLDER ./
+echo "Hello $1"
+time=$(date)
+echo ::set-output name=time::$time
